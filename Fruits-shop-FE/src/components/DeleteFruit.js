7e4575e2899444e9bbf1be361a7b62e99213690a
ameted83 <tedeschiamelia@gmail.com>
2022-08-04 12:24:30 +0200

    final project

--- Fruits-shop-FE/src/components/DeleteFruit.js
+++ Fruits-shop-FE/src/components/DeleteFruit.js
@@ -25,7 +25,7 @@ export default function DeleteFruit({ fruit, handleClose }) {
           </h2>
 
           <button
-            onClick={handleClose}
+            onClick={() => handleClose}
             type="submit"
             className=" mt-5 px-4 py-2 font-bold rounded shadow focus:outline-none hover:ring focus:ring-opacity-50 hover:ring-white bg-orange-500 mr-3"
           >
